3918bd74b6839c18dbb3dd565711744cd35599b8
EKreir <ekreir97@gmail.com>
2024-12-22 09:10:01 +0100

    add templates elements

---
+++ views/template/footer.php
@@ -0,0 +1,6 @@
+    </main>
+    <footer>
+        <p>&copy; 2024 Vet Clinic</p>
+    </footer>
+</body>
+</html>
---
+++ views/template/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion Admin</title>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+    <header>
+        <h1>Création de comptes du personnel</h1>
+    </header>
+    <main>
