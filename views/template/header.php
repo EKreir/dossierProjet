67ff84cc979ada70c9a168bb0cb05924d4a54ab5
EKreir <ekreir97@gmail.com>
2024-12-30 17:31:25 +0100

    update head links

--- views/template/header.php
+++ views/template/header.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion Admin</title>
-    <link rel="stylesheet" href="/css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="icon" href="/images/logozoo.jpg" sizes="16x16" type="image/jpg">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap&family=Roboto+Slab&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
     <header>
